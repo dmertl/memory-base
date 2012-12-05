3f078bafcacff21dc7d9143730298da2c3b52ded
David Mertl <dave@onzra.com>
2012-12-04 20:42:44 -0800

    Updating salts

--- app/Config/core.php
+++ app/Config/core.php
@@ -184,12 +184,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'alsdfjasfj0ajfoashfoasdhf9wf8023h80n2horiansdlndvnvnnvnvnvnvnnv');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '128378120937120937120973092173');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
